68cca2a6b55b2005245521717bdd9d633fa9f05a
Frances Huang <frances.y.huang@gmail.com>
2022-05-27 10:20:43 -0700

    fnished model user, joined_server_user

--- app/models/chat.py
+++ app/models/chat.py
@@ -3,23 +3,23 @@ from datetime import datetime
 
 class Chat(db.Model):
     __tablename__ = 'chats'
-    
+
     id = db.Column(db.Integer, primary_key=True)
     channel_id = db.Column(db.Integer, db.ForeignKey("channels.id"), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
     message = db.Column(db.Text(2000), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.now())
-    
+
     # Chat-to-Channel: many to one
     channel = db.relationship("Channel", back_populates="chats")
     # Chat-to-User: many to one
     user = db.relationship("User", back_populates="chats")
-    
+
     def to_dict(self):
         return {
             'id': self.id,
             'channel_id': self.channel_id,
             'user_id': self.user_id,
             'message': self.message,
-            'created_at': self.created_at,
+            'created_at': self.created_at.isoformat(),
         }
--- app/models/user.py
+++ app/models/user.py
@@ -10,6 +10,43 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(40), nullable=False, unique=True)
     email = db.Column(db.String(255), nullable=False, unique=True)
     hashed_password = db.Column(db.String(255), nullable=False)
+    avatar = db.Column(db.Text())
+
+    # created servers
+    servers = db.relationship("Server", back_populates="user", cascade="all, delete")
+
+    # created channels
+    channels = db.relationship("Channel", back_populates="user")
+
+    # joined servers
+    joined_servers = db.relationship("Server", \
+        secondary="join(JoinServerUser, Server, JoinServerUser.server_id == Server.id)", \
+        primaryjoin="User.id==JoinServerUser.user_id", viewonly=True)
+
+    # joined channels
+    joined_channels = db.relationship("Channel", \
+        secondary="join(JoinServerUser, Server, JoinServerUser.server_id == Server.id)." \
+                "join(Channel, Channel.server_id == Server.id)",\
+        primaryjoin="and_(User.id == JoinServerUser.user_id)", \
+        secondaryjoin="JoinServerUser.server_id == Server.id", viewonly=True)
+
+    # joined chats
+    joined_chats = db.relationship("Chat", \
+        secondary="join(JoinServerUser, Server, JoinServerUser.server_id == Server.id)." \
+                "join(Channel, Channel.server_id == Server.id)."
+                "join(Chat, Channel.id == Chat.channel_id)",\
+        primaryjoin="and_(User.id == JoinServerUser.user_id)", \
+        secondaryjoin="JoinServerUser.server_id == Server.id", viewonly=True)
+
+    #chats
+    chats = db.relationship("Chat", back_populates="user")
+
+    # private channels
+    private_channels = db.relationship("PrivateChannel", primaryjoin="or_(User.id==PrivateChannel.user_id, User.id==PrivateChannel.friend_id)", viewonly=True)
+
+    # private chats
+    private_chats = db.relationship("PrivateChat", back_populates="user")
+
 
     @property
     def password(self):
@@ -26,5 +63,6 @@ class User(db.Model, UserMixin):
         return {
             'id': self.id,
             'username': self.username,
-            'email': self.email
+            'email': self.email,
+            'avatar': self.avatar,
         }
