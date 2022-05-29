2615fa93fa1e27475046296963962c828269f261
Frances Huang <frances.y.huang@gmail.com>
2022-05-29 12:02:08 -0700

    merge

--- react-app/src/components/auth/LogoutButton.js
+++ react-app/src/components/auth/LogoutButton.js
@@ -4,14 +4,13 @@ import { useHistory } from "react-router-dom";
 import { logout } from "../../store/session";
 
 const LogoutButton = () => {
-  const dispatch = useDispatch();
-  const history = useHistory();
-  const onLogout = async (e) => {
-    await dispatch(logout());
-    history.push("/");
-  };
+	const dispatch = useDispatch();
+	const history = useHistory();
+	const onLogout = async (e) => {
+		await dispatch(logout());
+		history.push("/");
+	};
 
-<<<<<<< HEAD
 	return (
 		<div className="sidebar-btn-ctrl tooltip">
 			<div
@@ -24,19 +23,6 @@ const LogoutButton = () => {
 			<div className="tooltiptext">Logout</div>
 		</div>
 	);
-=======
-  return (
-    <div className="sidebar-btn-ctrl">
-      <div
-        id="logout"
-        onClick={onLogout}
-        className="sidebar-btn sidebar-btn-dark"
-      >
-        <i className="fa-solid fa-right-from-bracket sidebar-icon"></i>
-      </div>
-    </div>
-  );
->>>>>>> main
 };
 
 export default LogoutButton;
