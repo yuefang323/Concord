c37d0aab6c5240b7e8718b0b5f8564e10a8cc7e1
Frances Huang <frances.y.huang@gmail.com>
2022-06-01 20:04:18 -0700

    chat will immediatly render user

--- react-app/src/components/Main/Chat/Chats/index.js
+++ react-app/src/components/Main/Chat/Chats/index.js
@@ -1,5 +1,3 @@
-import { useEffect, useRef } from "react";
-
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 
@@ -8,19 +6,12 @@ import ChatDivs from "../Inputs/ChatDivs";
 const Chats = () => {
 	const channelId = parseInt(useParams().channelId);
 	const channel = useSelector((state) => state.channels.byId)[channelId];
-	const chats = useSelector((state) => state.chats.byId);
-
-	// const lastRef = useRef();
-
-	// useEffect(() => {
-	// focus on last div
-	// 	lastRef.current.focus();
-	// }, [chats]);
+	// const chats = useSelector((state) => state.chats.byId);
 
 	return (
 		<div className="chat-div-wrap">
 			{channel?.chats?.map((chatId) => (
-				<ChatDivs chat={chats[chatId]} key={chatId} />
+				<ChatDivs chatId={chatId} key={chatId} />
 			))}
 		</div>
 	);
--- react-app/src/components/Main/Chat/Inputs/ChatDivs.js
+++ react-app/src/components/Main/Chat/Inputs/ChatDivs.js
@@ -1,20 +1,29 @@
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 
 import Avatar from "./Avatar";
 import CreatedAt from "./CreatedAt";
 
-const ChatDivs = ({ chat }) => {
+const ChatDivs = ({ chatId }) => {
 	const users = useSelector((state) => state.users.byId);
+	const chats = useSelector((state) => state.chats.byId);
+	const [user, setUser] = useState();
+
+	useEffect(() => {
+		setUser(users[chats[chatId].user_id]);
+	}, [chats]);
 
 	return (
-		<div className="chat-div-ctrl" id={chat?.id}>
-			<Avatar user={users[chat?.user_id]} />
+		<div className="chat-div-ctrl" id={chatId}>
+			<Avatar user={user} />
 			<div className="chat-message-ctrl">
 				<div className="chat-user-wrap">
-					<div className="chat-username">{users[chat?.user_id]?.username}</div>
-					<CreatedAt created_at={chat?.created_at} />
+					<div className="chat-username">
+						{users[chats[chatId]?.user_id]?.username}
+					</div>
+					<CreatedAt created_at={chats[chatId]?.created_at} />
 				</div>
-				<div>{chat?.message}</div>
+				<div>{chats[chatId]?.message}</div>
 			</div>
 		</div>
 	);
