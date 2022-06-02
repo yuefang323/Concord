cf9b3fb1855824ba8c46340cd48c52a833c65885
Frances Huang <frances.y.huang@gmail.com>
2022-06-01 20:13:34 -0700

    auto scroll worksgit add .

--- react-app/src/components/Main/Chat/Chats/index.js
+++ react-app/src/components/Main/Chat/Chats/index.js
@@ -1,3 +1,4 @@
+import { useRef, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 
@@ -6,10 +7,21 @@ import ChatDivs from "../Inputs/ChatDivs";
 const Chats = () => {
 	const channelId = parseInt(useParams().channelId);
 	const channel = useSelector((state) => state.channels.byId)[channelId];
-	// const chats = useSelector((state) => state.chats.byId);
+	const chats = useSelector((state) => state.chats);
+
+	const focusRef = useRef();
+
+	useEffect(() => {
+		if (focusRef) {
+			focusRef.current.addEventListener("DOMNodeInserted", (e) => {
+				const { currentTarget: target } = e;
+				target.scroll({ top: target.scrollHeight, behavior: "smooth" });
+			});
+		}
+	}, []);
 
 	return (
-		<div className="chat-div-wrap">
+		<div className="chat-div-wrap" ref={focusRef}>
 			{channel?.chats?.map((chatId) => (
 				<ChatDivs chatId={chatId} key={chatId} />
 			))}
