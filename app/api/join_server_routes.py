51cb3882563865eb93260a1c94e3194029fb1c82
Frances Huang <frances.y.huang@gmail.com>
2022-06-04 13:25:45 -0700

    bug in join_server_routes.py

--- app/api/join_server_routes.py
+++ app/api/join_server_routes.py
@@ -48,7 +48,7 @@ def join_server():
         return {
             "joinServer": newJoin.to_dict(),
             "channels":[channel.to_dict() for channel in channels],
-            "chats": [chats.to_dict() for chat in chats],
+            "chats": [chat.to_dict() for chat in chats],
             "users": [user.to_dict_limited() for user in users]
         }
     return {'errors': validation_errors_to_error_messages(form.errors)}, 401
