619cc6e40755d10984a2c19954e0f2ebf554c89e
LincolnHer <lincolnher15@gmail.com>
2022-06-05 17:34:46 -0500

    fixed about me

--- react-app/src/components/Splash/About.js
+++ react-app/src/components/Splash/About.js
@@ -66,6 +66,13 @@ const About = () => {
 						>
 							<img src={git} alt="GitHub" className="about-icon" />
 						</a>
+						<a href="https://www.linkedin.com/in/fang-yue-7b3091241/" target="_blank" rel="noreferrer">
+                            <img
+                                src={linkedin}
+                                alt="Linked In"
+                                className="about-icon"
+                            />
+                        </a>
 						{/* <a href="#" target="_blank" rel="noreferrer">
 							<img src={linkedin} alt="Linked In" className="about-icon" />
 						</a> */}
@@ -95,16 +102,30 @@ const About = () => {
 					</div>
 				</div>
 				<div className="about-cards">
-					<div className="about-photo">Photo Here</div>
+					<div className="about-photo">
+						<img
+							src={lincoln}
+							alt='Lincoln'
+							className="about-icon lincoln"
+						/>
+					</div>
 					<div className="about-name">Lincoln Her</div>
-					<div className="about-slogan">Slogan</div>
+    				<div className="about-slogan">Develop success from failures.</div>
 					<div className="about-icon-wrap">
-						<a href="#" target="_blank" rel="noreferrer">
-							<img src={git} alt="GitHub" className="about-icon" />
-						</a>
-						<a href="#" target="_blank" rel="noreferrer">
-							<img src={linkedin} alt="Linked In" className="about-icon" />
-						</a>
+						<a href="https://github.com/LincolnHer" target="_blank" rel="noreferrer">
+                            <img
+                                src={git}
+                                alt="GitHub"
+                                className="about-icon"
+                            />
+                        </a>
+                        <a href="https://www.linkedin.com/in/lincoln-her/" target="_blank" rel="noreferrer">
+                            <img
+                                src={linkedin}
+                                alt="Linked In"
+                                className="about-icon"
+                            />
+                        </a>
 					</div>
 				</div>
 			</main>
