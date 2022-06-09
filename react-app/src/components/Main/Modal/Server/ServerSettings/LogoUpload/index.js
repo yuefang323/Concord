5d9ab906b7ddfe8b1f8843b2cabb7baffbb6269b
Frances H <97005157+frances-y-h@users.noreply.github.com>
2022-06-09 07:25:51 -0700

    Update index.js

--- react-app/src/components/Main/Modal/Server/ServerSettings/LogoUpload/index.js
+++ react-app/src/components/Main/Modal/Server/ServerSettings/LogoUpload/index.js
@@ -7,8 +7,8 @@ import * as serversActions from "../../../../../../store/servers";
 
 const S3_BUCKET = "go-concord";
 const REGION = "us-east-1";
-const ACCESS_KEY = "AKIASAMA5YUWC7FJFDHC";
-const SECRET_ACCESS_KEY = "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";
+const ACCESS_KEY = "";
+const SECRET_ACCESS_KEY = "";
 
 AWS.config.update({
 	accessKeyId: ACCESS_KEY,
