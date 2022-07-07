741b671f93f5337b55e1d503d67015a84b45022e
LincolnHer <lincolnher15@gmail.com>
2022-07-06 19:02:14 -0500

    adjusted css for no logo

--- react-app/src/components/Main/Modal/Server/EditUser/EditUserForm.js
+++ react-app/src/components/Main/Modal/Server/EditUser/EditUserForm.js
@@ -7,112 +7,117 @@ import { updateUser } from "../../../../../store/session";
 import "./EditUser.css";
 
 const EditUser = ({ onClose }) => {
-	const dispatch = useDispatch();
-	const user = useSelector((state) => state.session.user);
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.session.user);
 
-	const [username, setUsername] = useState(user?.username);
-	const [email, setEmail] = useState(user?.email);
-	const [avatar, setAvatar] = useState(user?.avatar);
-	const [errors, setErrors] = useState([]);
+  const [username, setUsername] = useState(user?.username);
+  const [email, setEmail] = useState(user?.email);
+  const [avatar, setAvatar] = useState(user?.avatar);
+  const [errors, setErrors] = useState([]);
 
-	const handleSubmit = async (e) => {
-		e.preventDefault();
-		const updatedUser = {
-			username: username,
-			email: email,
-			avatar: avatar,
-		};
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const updatedUser = {
+      username: username,
+      email: email,
+      avatar: avatar,
+    };
 
-		const data = await dispatch(updateUser(updatedUser, user?.id));
-		if (data.errors) {
-			setErrors(data.errors);
-		}
-	};
-
-    const cancel = (e) => {
-        setUsername(user?.username)
-        setEmail(user?.email)
-        setAvatar(user?.avatar)
-
+    const data = await dispatch(updateUser(updatedUser, user?.id));
+    if (data.errors) {
+      setErrors(data.errors);
     }
+  };
 
-	return (
-        <div className="edit-user-form">
-            <form onSubmit={handleSubmit}>
-                <div className="edit-modal-body">
-                    {user?.avatar ? (
-                        <img
-                            className="user-avatar form"
-                            src={user?.avatar}
-                            alt="user avatar"
-                        />
-                    ) : (
-                        <div className="avatar-bkg form">
-                            <img className="user-avatar no-logo" src={logo} alt="user avatar" />
-                        </div>
-                    )}
-                    <span className="username-body">{user?.username}</span>
-                </div>
-                <div>
-                    <div className="edit-modal-user-info-container">
-                        <div className="edit-modal-user-info">
-                            <div className="edit-info">
-                                <h5>USERNAME</h5>
-                                <input
-                                    className="edit-input"
-                                    type="text"
-                                    name="username"
-                                    value={username}
-                                    onChange={(e) => setUsername(e.target.value)}
-                                    // placeholder={user?.username}
-                                />
-                            </div>
-                            <div className="edit-info">
-                                <h5>EMAIL</h5>
-                                <input
-                                    className="edit-input"
-                                    type="email"
-                                    name="email"
-                                    value={email}
-                                    onChange={(e) => setEmail(e.target.value)}
-                                    // placeholder={user?.email}
-                                    disabled={user?.id === 1}
-                                />
-                            </div>
-                            <div className="edit-info">
-                                <h5>USER AVATAR</h5>
-                                <input
-                                    className="edit-input"
-                                    type="text"
-                                    name="avatar"
-                                    value={avatar}
-                                    onChange={(e) => setAvatar(e.target.value)}
-                                    // placeholder={user?.avatar}
-                                />
-                            </div>
-                        </div>
-                    </div>
-                    <ul className="errors">
-                        {errors.map((error) => (
-                            <li key={error}>{error}</li>
-                        ))}
-                    </ul>
-                </div>
-                <div className="edit-user-btns-container">
-                    <button
-                        className="edit-user-btns"
-                        type="button"
-                        onClick={cancel}
-                    >
-                        Cancel
-                    </button>
-                    <button className="edit-user-btns" type="submit">
-                        Save
-                    </button>
-                </div>
-            </form>
+  const cancel = (e) => {
+    setUsername(user?.username);
+    setEmail(user?.email);
+    setAvatar(user?.avatar);
+  };
+
+  return (
+    <div className="edit-user-form">
+      <h2 className="edit-user-title">My Account</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="edit-modal-body"></div>
+        <div className="edit-body-container">
+          {user?.avatar ? (
+            <img
+              className="user-avatar form"
+              src={user?.avatar}
+              alt="user avatar"
+            />
+          ) : (
+              <div className="avatar-bkg form">
+                <img
+                  className="user-avatar no-logo"
+                  src={logo}
+                  alt="user avatar"
+                />
+              </div>
+          )}
+          <span className="username-body">{user?.username}</span>
         </div>
-	);
+        <div>
+          <div className="edit-modal-user-info-container">
+            <div className="edit-modal-user-info">
+              <div className="edit-info">
+                <h5>USERNAME</h5>
+                <input
+                  className="edit-input"
+                  type="text"
+                  name="username"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                  // placeholder={user?.username}
+                />
+              </div>
+              <div className="edit-info">
+                <h5>EMAIL</h5>
+                <input
+                  className="edit-input"
+                  type="email"
+                  name="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  // placeholder={user?.email}
+                  disabled={user?.id === 1}
+                />
+              </div>
+              <div className="edit-info">
+                <h5>USER AVATAR</h5>
+                <input
+                  className="edit-input"
+                  type="text"
+                  name="avatar"
+                  value={avatar}
+                  onChange={(e) => setAvatar(e.target.value)}
+                  // placeholder={user?.avatar}
+                />
+              </div>
+            </div>
+          </div>
+          <ul className="errors">
+            {errors.map((error) => (
+              <li key={error}>{error}</li>
+            ))}
+          </ul>
+        </div>
+        <div className="edit-user-btns-container">
+          <button
+            className="form-create-btm-clear-btn"
+            type="button"
+            onClick={cancel}
+          >
+            Cancel
+          </button>
+          <button className="edit-user-btns" type="submit">
+            Save
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default EditUser;
